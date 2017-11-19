142847052df34fe44395ef09c5d36bb48b89eb8a
Zack <zack@zacklott.co.uk>
2017-11-19 21:17:45 +0000

    NEW: Config and Debug tools
    I've created a file where all the configuration will be stored and an area for debugging tools.
    
    [+] Config file
    [+] Dumpr

---
+++ bootstrap/config.php
@@ -0,0 +1,9 @@
+<?php
+    // Project Name
+    define('PROJECTNAME', 'YewTree');
+
+    // Database
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBNAME', 'YewTree');
